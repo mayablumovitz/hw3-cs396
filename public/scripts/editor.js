1240d832f16058e439ee21e2f1317ebb2caa75ea
mayablumovitz <63746979+mayablumovitz@users.noreply.github.com>
2021-05-13 16:22:00 -0500

    My second commit

--- public/scripts/editor.js
+++ public/scripts/editor.js
@@ -13,7 +13,8 @@ const initResetButton = () => {
         });
 };
 
-const getDoctors = () => {
+const getDoctors = (callback,newDocID) => {
+    console.log(newDocID);
     fetch(`${baseURL}/doctors`)
     .then(response => response.json())
     .then(data => {
@@ -29,7 +30,14 @@ const getDoctors = () => {
         document.querySelector("#doctor").innerHTML = "Selected doctor goes here.";
         document.querySelector("#companions").innerHTML = "Selected doctor's companions go here.";
     })
-    .then(attachEventHandlers);
+    .then(resp => {
+        attachEventHandlers();
+        if (callback) {
+            console.log("getDoctors callback");
+            callback(newDocID);
+        }
+    });
+    
 };
 
 const attachEventHandlers = () => {
@@ -43,19 +51,24 @@ const getDoctor = ev => {
     var id = ev.currentTarget.dataset.id;
     var doc = doctors.filter(doctor => doctor._id === id)[0];
     console.log(doc);
+    document.querySelector('#doctor').innerHTML = "";
     document.querySelector('#doctor').innerHTML = `
         <h2>${doc.name}</h2>
+        <button class="btn" id="edit" onclick = "editDoctor('${doc._id}')">Edit</button>
         <button class="btn" id="delete" onclick = "deleteDoctor('${doc._id}')">Delete</button>
         <p>Seasons: ${doc.seasons}</p>
         <img src="${doc.image_url}" />`;
     getCompanions(id);
 }
 
-async function getDoctorNoEv (id_) {
-    await getDoctors();
+const getDoctorNoEv = (id_) => {
+    console.log('step 2');
     var doc = doctors.filter((doctor) => doctor._id == id_)[0];
+    console.log('getDoctorNoEv found', doc);
+    document.querySelector('#doctor').innerHTML = "";
     document.querySelector('#doctor').innerHTML = `
         <h2>${doc.name}</h2>
+        <button class="btn" id="edit" onclick = "editDoctor('${doc._id}')">Edit</button>
         <button class="btn" id="delete" onclick = "deleteDoctor('${doc._id}')">Delete</button>
         <p>Seasons: ${doc.seasons}</p>
         <img src="${doc.image_url}" />`;
@@ -79,6 +92,7 @@ const getCompanions = id => {
 };
 
 const newDoctor = () => {
+    document.querySelector('#doctor').innerHTML = "";
     document.querySelector('#doctor').innerHTML = `
     <form>
         <section class="formClass1">
@@ -97,8 +111,36 @@ const newDoctor = () => {
             <label for="image_url">Image URL:</label>
             <input type="text" id="image_url" style = "width:280px;">
         </section>
-        <button class="btn btn-main" id="create" onclick = "saveDoctor()">Save</button>
-        <button class="btn" id="cancel" onclick = "getDoctors()">Cancel</button>
+        <button class="btn btn-main" id="create" type="button" onclick = "saveDoctor()">Save</button>
+        <button class="btn" id="cancel" type="button" onclick = "getDoctors()">Cancel</button>
+    </form>`
+    document.querySelector('#companions').innerHTML = ""
+}
+
+const editDoctor = id_ => {
+    console.log('is the problem here?');
+    var doc = doctors.filter((doctor) => doctor._id == id_)[0];
+    document.querySelector('#doctor').innerHTML = "";
+    document.querySelector('#doctor').innerHTML = `
+    <form>
+        <section class="formClass1">
+            <label for="name">Name:</label>
+            <input type="text" id="name" value = "${doc.name}" style = "width:280px;">
+        </section>
+        <section class="formClass2">
+            <label for="seasons">Seasons:</label>
+            <input type="text" id="seasons" value = "${doc.seasons}" style = "width:280px;">
+        </section>
+        <section class="formClass3">
+            <label for="ordering">Ordering:</label>
+            <input type="text" id="ordering" value = "${doc.ordering}" style = "width:280px;">
+        </section>
+        <section class="formClass4">
+            <label for="image_url">Image URL:</label>
+            <input type="text" id="image_url" value = "${doc.image_url}" style = "width:280px;">
+        </section>
+        <button class="btn btn-main" id="create" type="button" onclick = "sendEditedDoctor('${id_}')">Save</button>
+        <button class="btn" id="cancel" type="button" onclick = "getDoctorNoEv('${id_}')">Cancel</button>
     </form>`
     document.querySelector('#companions').innerHTML = ""
 }
@@ -141,15 +183,16 @@ const saveDoctor = ev => {
     })
     .then(resp => {
         console.log('Success:', resp);
-        return resp._id;
+        return resp;
     })
     // .then(resp => {
-    //     getDoctors();
-    //     return resp;
+    //     // console.log(resp);
+    //     // console.log(doctors);
+    //     // doctors.push(resp);
+    //     return ;
     // })
     .then(resp => {
-        console.log(resp);
-        getDoctorNoEv(resp);
+        getDoctors(getDoctorNoEv,resp._id);
     })
     .catch(err => {
         console.error(err);
@@ -185,5 +228,55 @@ const deleteDoctor = (ID) => {
         return;
     }
 };
+
+const sendEditedDoctor = id_ => {
+    const url = `/doctors/${id_}`;
+    const data = {
+        name: document.getElementById("name").value,
+        seasons: document.getElementById("seasons").value.split(','),
+        ordering: document.getElementById("ordering").value,
+        image_url: document.getElementById("image_url").value
+    }
+    if (data.name == "") {
+        console.error('Missing name!');
+        alert('Missing name!');
+        return;
+    }
+    if (data.seasons == "") {
+        console.error('Missing seasons!');
+        alert('Missing seasons!');
+        return;
+    }
+    else if (data.seasons.some(isNaN)) {
+        console.error('Seasons should be numbers seperated by commas!');
+        alert('Seasons should be numbers seperated by commas!');
+        return;
+    }
+    fetch(url, {
+            method: 'PATCH', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            } else {
+                return response.json();
+            }
+        })
+        .then(data => {
+            console.log('Success:', data);
+            return data
+        })
+        .then(resp => {
+            getDoctors(getDoctorNoEv,resp._id);
+        })
+        .catch(err => {
+            console.error(err);
+            alert('Error!');
+        });
+}
 // invoke this function when the page loads:
 getDoctors();
